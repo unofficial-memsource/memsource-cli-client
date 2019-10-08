6b57cf61a6e6bea1b28fc124cc9a6bde56a243ed
zerodayz <cerninr@gmail.com>
2019-10-08 00:39:30 +0000

    basic auth for jira

--- memsource_cli/scripts/README.md
+++ memsource_cli/scripts/README.md
@@ -1,22 +1,23 @@
 # Getting started
 
-### Prepare
+### Basic Authentication requirements
+
+You can skip `--user-name` and `--password` if you would like to, but please
+make sure to `export JIRA_USERNAME` and `export JIRA_PASSWORD` in your `rc` file
+
+For example:
 ```
-source ~/git/memsource-cli-client/.memsource/bin/activate
-pip install -r ~/git/memsource-cli-client/memsource_cli/scripts/requirements.txt
+export JIRA_USERNAME="jiraUser01"
+export JIRA_PASSWORD="My secret password"
 ```
 
-### Required Kerberos authentication
+#### Example: Basic Authentication
 ```
-kinit <user>@<DOMAIN>
-klist
-```
-
-### Create JIRA ticket
-```
-$ python3 jira.py --jira-url <JIRA URL> \
---auth-url <JIRA AUTH URL for Kerberos> \
---project <JIRA Project "PROJECT13> \
+python jira.py --jira-url <JIRA URL> \
+--basic \
+--user-name <JIRA username> \
+--password <JIRA password> \
+--project <JIRA Project "PROJECT"> \
 --summary <Summary> \
 --description <Description> \
 --labels <Labels "label1" "label2"> \
@@ -28,8 +29,41 @@ $ python3 jira.py --jira-url <JIRA URL> \
 --sprint <Sprint ID ie 7731>
 ```
 
+### Kerberos Authentication requirements
+```
+source ~/git/memsource-cli-client/.memsource/bin/activate
+pip install -r ~/git/memsource-cli-client/memsource_cli/scripts/requirements.txt
+kinit <user>@<DOMAIN>
+klist
+```
+
+#### Example: Kerberos Authentication
+```
+$ python jira.py --jira-url <JIRA URL> \
+--auth-url <JIRA AUTH URL for Kerberos> \
+--project <JIRA Project "PROJECT"> \
+--summary <Summary> \
+--description <Description> \
+--labels <Labels "label1" "label2"> \
+--components <Components "component1" "component2"> \
+--issue-type <Issue type Task, Bug...>
+--affects-version <Affects version ie "1.1.0" "1.0.0"> \
+--fix-versions <Fix version/s ie "1.2.0" "1.2.1"> \
+--epic <Epic ie PROJECT-12> \
+--sprint <Sprint ID ie 7731>
+```
+
+### Known issues
+
 #### How to get Sprint ID
 Find another JIRA issue in the same Sprint you want to use
 
 Visit  `https://<jira-url>/rest/api/2/issue/<jira-issue>`  
 Search for `com.atlassian.greenhopper.service.sprint.Sprint@166e2f31[id=7731,rapidViewId=...` and use id=`7731`
+
+#### Ignore Python SSL Warnings
+
+Add to your `rc` file:
+```
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+```
--- memsource_cli/scripts/jira.py
+++ memsource_cli/scripts/jira.py
@@ -5,9 +5,21 @@
 
 import requests
 import argparse
-from requests_kerberos import HTTPKerberosAuth
 import sys
 import json
+import os
+
+
+def env(*vars, **kwargs):
+    """Search for the first defined of possibly many env vars
+    Returns the first environment variable defined in vars, or
+    returns the default defined in kwargs.
+    """
+    for v in vars:
+        value = os.environ.get(v, None)
+        if value:
+            return value
+    return kwargs.get('default', '')
 
 
 def parse_args():
@@ -15,11 +27,27 @@ def parse_args():
                                      description="Create task in JIRA via REST API")
     parser.add_argument("--jira-url",
                         dest='jira_url',
-                        help="JIRA REST API URL")
+                        help="JIRA REST API URL",
+                        required=True)
     parser.add_argument("--auth-url",
                         dest='auth_url',
-                        required=True,
-                        help="JIRA REST API AUTH URL")
+                        help="JIRA REST API Kerberos URL")
+    parser.add_argument("--basic",
+                        dest='basic',
+                        action='store_true',
+                        help="JIRA basic authentication")
+    parser.add_argument("--no-ssl",
+                        dest='ssl',
+                        action='store_false',
+                        help="JIRA turn off SSL verification")
+    parser.add_argument("--user-name",
+                        dest='username',
+                        default=env('JIRA_USERNAME'),
+                        help="JIRA username")
+    parser.add_argument("--password",
+                        dest='password',
+                        default=env('JIRA_PASSWORD'),
+                        help="JIRA password")
     parser.add_argument("--project",
                         dest='project',
                         required=True,
@@ -67,31 +95,41 @@ def parse_args():
 class Jira:
     def __init__(self):
         self.args = parse_args()
-        self.kerberos_auth = HTTPKerberosAuth(
-            force_preemptive=True, mutual_authentication=False)
 
 
 if __name__ == '__main__':
     jira = Jira()
 
+    s = requests.Session()
+
+    if jira.args.auth_url:
+        print('Using GSSAPI Authentication')
+        from requests_kerberos import HTTPKerberosAuth
+        kerberos_auth = HTTPKerberosAuth(
+            force_preemptive=True, mutual_authentication=False)
+        response = s.get(
+            jira.args.auth_url, auth=kerberos_auth, verify=jira.args.ssl)
+
+    elif jira.args.basic and not jira.args.username and not jira.args.password:
+        print('Using Basic Authentication')
+        print('With \'--basic\'. You must specify \'--user-name\' and \'---password\'.')
+        sys.exit(1)
+
+    elif jira.args.basic and jira.args.username and jira.args.password:
+        print('Using Basic Authentication')
+        basic_auth = requests.auth.HTTPBasicAuth(
+            jira.args.username, jira.args.password)
+        response = s.get(
+            jira.args.jira_url, auth=basic_auth, verify=jira.args.ssl)
+
+    cookies = response.cookies
+
     _components = []
     for i in jira.args.components:
         _components.append({'name': i})
 
-    _fixversions = []
-    for i in jira.args.fix_versions:
-        _fixversions.append({'name': i})
-
-    _versions = []
-    for i in jira.args.affects_versions:
-        _versions.append({'name': i})
-
-    s = requests.Session()
-    response = s.get(
-        jira.args.auth_url, auth=jira.kerberos_auth, verify=False)
-    cookies = response.cookies
     response = s.post(
-        jira.args.jira_url + '/rest/api/2/issue', cookies=cookies, verify=False,
+        jira.args.jira_url + '/rest/api/2/issue', cookies=cookies, verify=jira.args.ssl,
         json={
             "fields": {
                 "project":
@@ -109,37 +147,53 @@ if __name__ == '__main__':
         headers={
             "Content-Type": "application/json"
         })
+
     issue = json.loads(response.content).get("key")
     print(jira.args.jira_url + '/browse/' + issue)
-    response = s.put(
-        jira.args.jira_url + '/rest/api/2/issue/' + issue, cookies=cookies, verify=False,
-        json={
-            "fields": {
-                "fixVersions": _fixversions,
-                "versions": _versions,
-            }},
-        headers={
-            "Content-Type": "application/json"
-        })
-    response = s.post(
-        jira.args.jira_url + '/rest/agile/1.0/epic/' +
-        jira.args.epic + '/issue', cookies=cookies, verify=False,
-        json={
-            "issues": [
-                issue
-            ]
-        },
-        headers={
-            "Content-Type": "application/json"
-        })
-    response = s.post(
-        jira.args.jira_url + '/rest/agile/1.0/sprint/' +
-        jira.args.sprint + '/issue', cookies=cookies, verify=False,
-        json={
-            "issues": [
-                issue
-            ]
-        },
-        headers={
-            "Content-Type": "application/json"
-        })
+
+    if jira.args.fix_versions and jira.args.affects_versions:
+
+        _fixversions = []
+        for i in jira.args.fix_versions:
+            _fixversions.append({'name': i})
+
+        _versions = []
+        for i in jira.args.affects_versions:
+            _versions.append({'name': i})
+
+        response = s.put(
+            jira.args.jira_url + '/rest/api/2/issue/' + issue, cookies=cookies,
+            verify=jira.args.ssl,
+            json={
+                "fields": {
+                    "fixVersions": _fixversions,
+                    "versions": _versions,
+                }},
+            headers={
+                "Content-Type": "application/json"
+            })
+
+    if jira.args.epic:
+        response = s.post(
+            jira.args.jira_url + '/rest/agile/1.0/epic/' +
+            jira.args.epic + '/issue', cookies=cookies, verify=jira.args.ssl,
+            json={
+                "issues": [
+                    issue
+                ]
+            },
+            headers={
+                "Content-Type": "application/json"
+            })
+    if jira.args.sprint:
+        response = s.post(
+            jira.args.jira_url + '/rest/agile/1.0/sprint/' +
+            jira.args.sprint + '/issue', cookies=cookies, verify=jira.args.ssl,
+            json={
+                "issues": [
+                    issue
+                ]
+            },
+            headers={
+                "Content-Type": "application/json"
+            })
